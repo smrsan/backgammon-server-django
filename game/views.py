e6e570810c58ff613bfd8d693ae54fdc99371d42
smrsan <smrsan@users.noreply.github.com>
2020-01-12 01:39:54 +0330

    Added basic view-sets to views.py

--- game/views.py
+++ game/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Game, Board, Turn
+from .serializers import UserSerializer, GameSerializer, BoardSerializer, TurnSerializer
+from .permissions import HasAccessToGame, HasAccessToGameRelatedModels
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class GameViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [IsAuthenticated, HasAccessToGame]
+
+
+class BoardViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated, HasAccessToGameRelatedModels]
+
+
+class TurnViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Turn.objects.all()
+    serializer_class = TurnSerializer
+    permission_classes = [IsAuthenticated, HasAccessToGameRelatedModels]
