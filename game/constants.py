74a7bd68ef7befe164bc3889719de12e72cca7d0
smrsan <smrsan@users.noreply.github.com>
2020-01-12 05:34:20 +0330

    Add constants.py to game

---
+++ game/constants.py
@@ -0,0 +1,80 @@
+# Position Meanings:
+# 0 -> Hitted
+# 1-24 -> On Board
+# 25 -> Beared Off
+
+# Backgammon board pattern is like this image:
+# https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Bg-movement.svg/1024px-Bg-movement.svg.png
+
+
+# start = 24 -> 1
+# end   = 1 -> 24
+
+# Default
+black_home_start = {
+    "black_bead_01_position": 6,
+    "black_bead_02_position": 6,
+    "black_bead_03_position": 6,
+    "black_bead_04_position": 6,
+    "black_bead_05_position": 6,
+    "black_bead_06_position": 8,
+    "black_bead_07_position": 8,
+    "black_bead_08_position": 8,
+    "black_bead_09_position": 13,
+    "black_bead_10_position": 13,
+    "black_bead_11_position": 13,
+    "black_bead_12_position": 13,
+    "black_bead_13_position": 13,
+    "black_bead_14_position": 24,
+    "black_bead_15_position": 24,
+
+    "white_bead_01_position": 19,
+    "white_bead_02_position": 19,
+    "white_bead_03_position": 19,
+    "white_bead_04_position": 19,
+    "white_bead_05_position": 19,
+    "white_bead_06_position": 17,
+    "white_bead_07_position": 17,
+    "white_bead_08_position": 17,
+    "white_bead_09_position": 12,
+    "white_bead_10_position": 12,
+    "white_bead_11_position": 12,
+    "white_bead_12_position": 12,
+    "white_bead_13_position": 12,
+    "white_bead_14_position": 1,
+    "white_bead_15_position": 1,
+}
+
+black_home_end = {
+    "black_bead_01_position": 19,
+    "black_bead_02_position": 19,
+    "black_bead_03_position": 19,
+    "black_bead_04_position": 19,
+    "black_bead_05_position": 19,
+    "black_bead_06_position": 17,
+    "black_bead_07_position": 17,
+    "black_bead_08_position": 17,
+    "black_bead_09_position": 12,
+    "black_bead_10_position": 12,
+    "black_bead_11_position": 12,
+    "black_bead_12_position": 12,
+    "black_bead_13_position": 12,
+    "black_bead_14_position": 1,
+    "black_bead_15_position": 1,
+
+    "white_bead_01_position": 6,
+    "white_bead_02_position": 6,
+    "white_bead_03_position": 6,
+    "white_bead_04_position": 6,
+    "white_bead_05_position": 6,
+    "white_bead_06_position": 8,
+    "white_bead_07_position": 8,
+    "white_bead_08_position": 8,
+    "white_bead_09_position": 13,
+    "white_bead_10_position": 13,
+    "white_bead_11_position": 13,
+    "white_bead_12_position": 13,
+    "white_bead_13_position": 13,
+    "white_bead_14_position": 24,
+    "white_bead_15_position": 24,
+}
