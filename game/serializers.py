075dd4811db76456b7b6c518b9abcf5d3b0d5767
smrsan <smrsan@users.noreply.github.com>
2020-01-12 05:35:34 +0330

    Define create() for GameSerializer

--- game/serializers.py
+++ game/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import Game, Board, Turn
+from .constants import black_home_end
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -44,6 +45,7 @@ class GameSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(source='id')
     created = serializers.ReadOnlyField(source='created')
     started = serializers.ReadOnlyField(source='started')
+    ended = serializers.ReadOnlyField(source='ended')
     owner = serializers.ReadOnlyField(source='owner.username')
     opponent = serializers.ReadOnlyField(source='opponent.username')
     winner = serializers.ReadOnlyField(source='winner.username')
@@ -54,10 +56,28 @@ class GameSerializer(serializers.ModelSerializer):
             'id',
             'created',
             'started',
+            'ended',
             'private',
             'owner',
             'opponent',
             'winner',
             'is_owner_black',
-            'is_owner_home_right'
+            'is_owner_home_start'
         ]
+
+    def create(self, validated_data):
+        other_game = Game.objects.get(
+            owner=self.context['request'].user, ended=None)
+
+        game = Game.objects.create(**validated_data)
+
+        is_owner_black = validated_data.get('is_owner_black', True)
+        is_owner_home_start = validated_data.get('is_owner_home_start', True)
+
+        if is_owner_black and not is_owner_home_start or \
+                not is_owner_black and is_owner_home_start:
+            Board.objects.create(game=game, **black_home_end)
+        else:
+            Board.objects.create(game=game)
+
+        return game
