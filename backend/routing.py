02eee8fea2c4aa0e40b333a35b0bb09d7b444230
smrsan <smrsan@users.noreply.github.com>
2020-01-27 08:49:27 +0330

    Use drf-simplejwt for django channels auth

--- backend/routing.py
+++ backend/routing.py
@@ -2,14 +2,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
+from .channelsmiddleware import TokenAuthMiddleware
 from game import routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
+            TokenAuthMiddleware(
+                URLRouter(
+                    routing.websocket_urlpatterns
+                )
             )
         )
     )
