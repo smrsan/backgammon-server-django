6638e88537550f452cbddb14a659f89cd5b26cde
smrsan <smrsan@users.noreply.github.com>
2020-01-27 04:15:09 +0330

    Install/Setup django-cors-headers

--- backend/settings.py
+++ backend/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
 
     'game',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS Headers
+CORS_ORIGIN_ALLOW_ALL = True
 
 # REST Framework
 REST_FRAMEWORK = {
